05bcc37253cb32e405e33ba18d43770e62a33f2e
Jimena <jparatge@gmail.com>
2024-09-26 22:58:19 +0000

    segunda version

--- src/app.js
+++ src/app.js
@@ -5,11 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// window.onload = function() {
-//   let cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "Q", "K"];
-// };
-// function elegirAleatorio (){
-// Math.floor
-// return cardValues [Math.floor (Math.random)*cardValues.length)];
-// }
-// const valorAleatorio = elegirAleatorio ();
+window.onload = function() {
+  let cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "Q", "K"];
+  let pintas = ["♦", "♥", "♠", "♣"];
+
+  function elegirAleatorio(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+  const valorAleatorio = elegirAleatorio(cardValues);
+  document.querySelector(".value").innerHTML = valorAleatorio;
+
+  const pintaAleatoria1 = elegirAleatorio(pintas);
+  document.querySelector(".pinta1").innerHTML = pintaAleatoria1;
+  document.querySelector(".pinta2").innerHTML = pintaAleatoria1;
+  document.querySelector(".pinta3").innerHTML = pintaAleatoria1;
+  document.querySelector(".pinta4").innerHTML = pintaAleatoria1;
+};
