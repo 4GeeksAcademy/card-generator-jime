32e24928290a33fc6a9dad91ac9048d5d41d6931
Jimena <jparatge@gmail.com>
2024-09-28 23:35:24 +0000

    tercer version

--- src/app.js
+++ src/app.js
@@ -15,9 +15,11 @@ window.onload = function() {
   const valorAleatorio = elegirAleatorio(cardValues);
   document.querySelector(".value").innerHTML = valorAleatorio;
 
-  const pintaAleatoria1 = elegirAleatorio(pintas);
-  document.querySelector(".pinta1").innerHTML = pintaAleatoria1;
-  document.querySelector(".pinta2").innerHTML = pintaAleatoria1;
-  document.querySelector(".pinta3").innerHTML = pintaAleatoria1;
-  document.querySelector(".pinta4").innerHTML = pintaAleatoria1;
+  const pintaAleatoria = elegirAleatorio(pintas);
+  document.querySelectorAll(".pinta").forEach(elementoPinta => {
+    elementoPinta.innerHTML = pintaAleatoria;
+    if (pintaAleatoria == "♥" || pintaAleatoria === "♦") {
+      elementoPinta.style.color = "red";
+    }
+  });
 };
