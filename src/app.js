b45c50bf870ba42453a0136ce8d3d67ef18d3912
Jimena <jparatge@gmail.com>
2024-10-01 03:27:02 +0000

    4ta version

--- src/app.js
+++ src/app.js
@@ -4,22 +4,41 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+let customHeigth = 400;
+let intervalId;
 window.onload = function() {
-  let cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "Q", "K"];
-  let pintas = ["♦", "♥", "♠", "♣"];
-
-  function elegirAleatorio(array) {
-    return array[Math.floor(Math.random() * array.length)];
-  }
-  const valorAleatorio = elegirAleatorio(cardValues);
-  document.querySelector(".value").innerHTML = valorAleatorio;
-
-  const pintaAleatoria = elegirAleatorio(pintas);
-  document.querySelectorAll(".pinta").forEach(elementoPinta => {
-    elementoPinta.innerHTML = pintaAleatoria;
-    if (pintaAleatoria == "♥" || pintaAleatoria === "♦") {
-      elementoPinta.style.color = "red";
-    }
+  document.querySelector("button").addEventListener("click", generarCartaNueva);
+  document.querySelector("#button1").addEventListener("click", () => {
+    intervalId = setInterval(generarCartaNueva, 10000);
   });
+  document.querySelector("#button2").addEventListener("click", () => {
+    clearInterval(intervalId);
+  });
+  document.querySelector("#inputHeigth").addEventListener("change", event => {
+    customHeigth = event.target.value;
+    document.querySelector(".card").style.heigth = customHeigth + "px";
+  });
+  let cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "Q", "K"];
+
+  function generarCartaNueva() {
+    let pintas = ["♦", "♥", "♠", "♣"];
+
+    function elegirAleatorio(array) {
+      return array[Math.floor(Math.random() * array.length)];
+    }
+
+    const valorAleatorio = elegirAleatorio(cardValues);
+    document.querySelector(".value").innerHTML = valorAleatorio;
+
+    const pintaAleatoria = elegirAleatorio(pintas);
+    document.querySelectorAll(".pinta").forEach(elementoPinta => {
+      elementoPinta.innerHTML = pintaAleatoria;
+      if (pintaAleatoria == "♥" || pintaAleatoria === "♦") {
+        elementoPinta.style.color = "red";
+      } else {
+        elementoPinta.style.color = "black";
+      }
+    });
+  }
+  generarCartaNueva();
 };
