67100cbd058285c51174206699c9e45b2cc17b1e
Jimena <jparatge@gmail.com>
2024-10-01 23:00:27 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -4,20 +4,29 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-let customHeigth = 400;
+let customHeight = 400;
+let customWidth = 300;
 let intervalId;
 window.onload = function() {
-  document.querySelector("button").addEventListener("click", generarCartaNueva);
+  document
+    .querySelector("#button")
+    .addEventListener("click", generarCartaNueva);
   document.querySelector("#button1").addEventListener("click", () => {
     intervalId = setInterval(generarCartaNueva, 10000);
   });
   document.querySelector("#button2").addEventListener("click", () => {
     clearInterval(intervalId);
   });
-  document.querySelector("#inputHeigth").addEventListener("change", event => {
-    customHeigth = event.target.value;
-    document.querySelector(".card").style.heigth = customHeigth + "px";
+  document.querySelector("#inputHeight").addEventListener("change", event => {
+    customHeight = event.target.value;
+    document.querySelector(".card").style.height = customHeight + "px";
   });
+
+  document.querySelector("#inputWidth").addEventListener("change", event => {
+    customWidth = event.target.value;
+    document.querySelector(".card").style.width = customWidth + "px";
+  });
+
   let cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "Q", "K"];
 
   function generarCartaNueva() {
